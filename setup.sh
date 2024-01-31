e0ee23601e40a2100223b101373e7221b947ecfd
Karol Chmurzyński <chmuri@gmail.com>
2024-01-31 17:37:38 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Function to add lines to a specified RC file
+add_lines_to_rc_file() {
+    rc_file="$1"
+    lines=("export VAGRANT_DEFAULT_PROVIDER=libvirt" "export VAGRANT_PREFERRED_PROVIDERS=libvirt")
+
+    # Iterate over the lines to add
+    for line in "${lines[@]}"; do
+        # Check if the line already exists in the RC file
+        grep -qF "$line" "$rc_file" || echo "$line" >> "$rc_file"
+    done
+}
+
+# Check and add lines to .bashrc if it exists
+if [ -f "$HOME/.bashrc" ]; then
+    add_lines_to_rc_file "$HOME/.bashrc"
+fi
+
+# Check and add lines to .zshrc if it exists
+if [ -f "$HOME/.zshrc" ]; then
+    add_lines_to_rc_file "$HOME/.zshrc"
+fi
+
+# Install necessary packages using apt
+sudo apt -y install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils vagrant libvirt-dev virt-manager
+
+# Install the Vagrant libvirt plugin
+vagrant plugin install vagrant-libvirt
+
+# Add the user to kvm and libvirt groups
+sudo usermod --append --groups kvm,libvirt "${USER}"
+
+# Set ownership and permissions for /dev/kvm
+sudo chown root:kvm /dev/kvm
+sudo chmod 660 /dev/kvm
+
+# Restart libvirtd and virtlogd services
+sudo systemctl restart libvirtd
+sudo systemctl restart virtlogd
